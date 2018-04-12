95b465a4404f7e3925c5eb336c246a5df3ffe4f3
pymd <pymd.work@gmail.com>
2018-04-12 19:52:38 +0530

    add boilerplate for patientDetail screen

---
+++ app/components/PatientDetailCard/PatientDetailCard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Card } from 'react-native-elements';
+
+const PatientDetailCard = () => {
+    return (
+        <Card />
+    );
+};
+
+export { PatientDetailCard };
---
+++ app/screens/PatientDetailScreenContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { PatientDetailScreen } from '../components/PatientDetailScreen';
+
+class PatientDetailScreenContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+}
+
+export default PatientDetailScreenContainer;
