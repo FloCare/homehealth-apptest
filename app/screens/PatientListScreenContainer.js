43d66d76d35e1d828e946c71068d922e4c6d2ea6
pymd <pymd.work@gmail.com>
2018-04-13 13:14:19 +0530

    checkpoint for merging with master

---
+++ app/screens/PatientListScreenContainer.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { PatientListScreen } from '../components/PatientListScreen';
+
+class PatientListScreenContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            patientList: {}
+        };
+    }
+
+    render() {
+        return (
+            <PatientListScreen
+                patientList={this.state.patientList}
+            />
+        );
+    }
+}
+
+export default PatientListScreenContainer;
