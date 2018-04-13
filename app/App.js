cb882caa9a26ad9754b1e4fe69bced4d5cc9f39c
harshal <harshalbid271@gmail.com>
2018-04-13 13:12:37 +0530

    Minor boot time fix

--- app/App.js
+++ app/App.js
@@ -1,17 +1,10 @@
 import {Navigation} from 'react-native-navigation';
-import NewPatient from './components/NewPatient';
-import {RegisterScreens} from './screens'
+import {RegisterScreens} from './screens';
 
 RegisterScreens();
-Navigation.registerComponent('name', () => NewPatient);
 
 Navigation.startTabBasedApp({
     tabs: [
-        {
-            label: 'one',
-            icon: require('../resources/ic_fiber_pin_2x.png'),
-            screen: 'name'
-        },
         {
             label: 'two',
             icon: require('../resources/ic_fiber_pin_2x.png'),
