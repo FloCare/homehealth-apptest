97e85792d83777f10a9c11d2a08b5bf197fc835c
bvr <balchandervr@gmail.com>
2018-04-18 11:10:15 +0530

    Added styles to PatientDetailCard

--- app/components/PatientDetailCard/styles.js
+++ app/components/PatientDetailCard/styles.js
@@ -1,14 +1,50 @@
 const styles = {
     parentContainerStyle: {
-
+        flex: 1
     },
     callButtonStyle: {
-        backgroundColor: 'rgba(92,216,10,1)',
-        width: 50,
-        height: 30,
-        borderColor: 'transparent',
-        borderWidth: 0,
-        borderRadius: 10
+        width: 72,
+        height: 40,
+        borderRadius: 21,
+        backgroundColor: 'transparent',
+        shadowColor: "rgba(0, 0, 0, 0.1)",
+        shadowOffset: {
+            width: 0,
+            height: 0
+        },
+        shadowRadius: 12,
+        shadowOpacity: 1,
+        borderStyle: "solid",
+        borderWidth: 1,
+        borderColor: "#45ceb1"
+    },
+    containerStyle: {
+        margin: 14,
+        marginLeft: 27,
+        marginRight: 16,
+        flexDirection: 'row'
+    },
+    iconStyle: {
+        alignSelf: 'flex-start',
+        margin: 2,
+        padding: 2
+    },
+    visitStyle: {
+        fontSize: 12,
+        width: 150,
+        lineHeight: 16,
+        letterSpacing: -0.26,
+        textAlign: "left",
+        color: "#999999"
+    },
+    headerStyle: {
+        fontSize: 18,
+        color: '#222222'
+    },
+    dividerStyle: {
+        margin: 0,
+        height: 1,
+        backgroundColor: "#dddddd"
     },
     itemStyle: {
         backgroundColor: '#f5f5f5',
@@ -20,16 +56,22 @@ const styles = {
         borderWidth: 0.5,
         borderColor: '#cccccc'
     },
+    noteStyle: {
+        fontSize: 12,
+        color: "#525252"
+    },
     buttonContainerStyle: {
+        position: 'absolute',
+        bottom: 4,
         flex: 1,
         flexDirection: 'row',
-        justifyContent: 'flex-start'
+        justifyContent: 'space-around'
     },
     footerButtonStyle: {
-        backgroundColor: 'rgba(92,216,10,1)',
-        borderColor: 'transparent',
-        borderWidth: 0,
-        borderRadius: 10
+        width: 180,
+        height: 40,
+        borderRadius: 3,
+        backgroundColor: "#45ceb1"
     }
 };
 
