e2e07e710f79b420acf2e45fa70c3a78a8ad8539
harshal <harshalbid271@gmail.com>
2018-04-23 09:49:29 +0530

    Introducing maps - checkpoint 2

--- app/components/common/PatientMap/PatientMapContainer.js
+++ app/components/common/PatientMap/PatientMapContainer.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import MapView, {Marker} from 'react-native-maps';
+import Polyline from '@mapbox/polyline';
 import {MapMarker} from './MapMarker';
 
 class PatientMapContainer extends Component {
@@ -8,9 +9,30 @@ class PatientMapContainer extends Component {
         patientLocation: {
             latitude: 37.78825,
             longitude: -122.4324
-        }
+        },
+        polylineCoordinates: []
     };
     // state = {patientAddress: props.patientAddress, patientLocation: props.patientLocation};
+
+    componentDidMount() {
+        this.getDirections('37.4, -122', `${this.state.patientLocation.latitude},${this.state.patientLocation.longitude}`);
+    }
+
+    async getDirections(startLoc, destinationLoc) {
+        try {
+            const resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc}&destination=${destinationLoc}`);
+            const respJson = await resp.json();
+            const points = Polyline.decode(respJson.routes[0].overview_polyline.points);
+            const coords = points.map((point) => ({
+                    latitude: point[0],
+                    longitude: point[1]
+                }));
+            this.setState({polylineCoordinates: coords});
+            return coords;
+        } catch (error) {
+            return error;
+        }
+    }
 
     render() {
         return (
@@ -19,6 +41,11 @@ class PatientMapContainer extends Component {
                 showsUserLocation
                 style={{flex: 1}}
             >
+                <MapView.Polyline
+                    coordinates={this.state.polylineCoordinates}
+                    strokeWidth={3}
+                    strokeColor="blue"
+                />
                 <Marker coordinate={this.state.patientLocation}>
                     <MapMarker text={this.state.patientAddress} />
                 </Marker>
