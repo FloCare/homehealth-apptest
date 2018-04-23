944ed482c9dbd442b4e1bf1b304b1cfd972dbc17
harshal <harshalbid271@gmail.com>
2018-04-23 19:27:54 +0530

    Added calendar strip to home screen

--- app/components/HomeScreen/HomeScreen.js
+++ app/components/HomeScreen/HomeScreen.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import CalendarStrip from 'react-native-calendar-strip';
+import {View} from 'react-native';
 import {VisitSummary} from './VisitSummary';
 
 function HomeScreen(props) {
     return (
         //TODO insert other stuff thats got to be on the home screen too
-        <VisitSummary
-            visitResultObject={props.visitResultObject}
-            navigateToVisitListScreen={props.navigateToVisitListScreen}
-            navigateToVisitMapScreen={props.navigateToVisitMapScreen}
-        />
+        <View style={{flex: 1}}>
+            <CalendarStrip
+                style={{height: 100, paddingTop: 20, paddingBottom: 10}}
+            />
+            <VisitSummary
+                visitResultObject={props.visitResultObject}
+                navigateToVisitListScreen={props.navigateToVisitListScreen}
+                navigateToVisitMapScreen={props.navigateToVisitMapScreen}
+            />
+        </View>
     );
 }
 
