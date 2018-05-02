d5f6162347a7b91ba32236dfe18c89b98ae02302
pymd <pymd.work@gmail.com>
2018-05-02 17:51:50 +0530

    notes screens stylings fixed

--- app/components/AddNoteForm/AddNoteFormWithPatientTag.js
+++ app/components/AddNoteForm/AddNoteFormWithPatientTag.js
@@ -8,29 +8,40 @@ import styles from './styles';
 const AddNoteFormWithPatientTag = (props) => {
     const {handleSubmit, name, onChangeText, value} = props;
     return (
-        <View style={{flex: 1}}>
+        <View style={{flex: 10, justifyContent: 'space-between'}}>
             <View style={{backgroundColor: '#e1e8ee', borderWidth: 1, borderColor: '#a2a2a2', height: 50}}>
-                <Text style={{flex: 1, textAlign: 'left', textAlignVertical: 'center', fontWeight: '200', fontSize: 25}}>{name}</Text>
-            </View>
-            <KeyboardAwareScrollView style={{flex: 1}} keyboardShouldPersistTaps='handled'>
-                <View
+                <Text
                     style={{
-                        borderColor: '#a2a2a2',
-                        borderBottomWidth: 1,
-                        backgroundColor: '#e1e8ee'
+                        flex: 1,
+                        textAlign: 'left',
+                        textAlignVertical: 'center',
+                        fontWeight: '200',
+                        fontSize: 23,
+                        paddingLeft: 10,
+                        paddingRight: 10
                     }}
-                >
-                    <TextInput
-                        style={{textAlignVertical: 'top'}}
-                        placeholder='Please type your notes here ...'
-                        multiline
-                        numberOfLines={25}
-                        onChangeText={onChangeText}
-                        value={value}
-                    />
-                </View>
+                >{name}</Text>
+            </View>
+            <KeyboardAwareScrollView
+                style={{
+                    flex: 9,
+                    borderColor: '#a2a2a2',
+                    borderBottomWidth: 1,
+                    backgroundColor: '#e1e8ee',
+                }}
+                keyboardShouldPersistTaps='handled'
+            >
+                <TextInput
+                    style={{textAlignVertical: 'top', paddingLeft: 10, paddingRight: 10}}
+                    placeholder='Please type your notes here ...'
+                    multiline
+                    numberOfLines={25}
+                    onChangeText={onChangeText}
+                    value={value}
+                />
             </KeyboardAwareScrollView>
             <Button
+                containerViewStyle={{marginLeft: 0, marginRight: 0}}
                 buttonStyle={styles.buttonStyle}
                 title='Save'
                 onPress={handleSubmit}
