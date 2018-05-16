4a33662a4a4fdc8b7bc2f516feda910dc6442226
harshal <harshal@flocare.health>
2018-05-16 18:55:56 +0530

    Changing app name from mtp to FloCare on iOS

---
+++ ios/FloCare/AppDelegate.m
@@ -0,0 +1,29 @@
+#import "AppDelegate.h"
+#import <React/RCTBundleURLProvider.h>
+
+#import "RCCManager.h"
+
+#import <React/RCTRootView.h>
+
+@import GoogleMaps;
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  [GMSServices provideAPIKey:@"AIzaSyBA9hAF2Roi0VeOgq10xIWEJnCZrsKn1iY"];
+
+  NSURL *jsCodeLocation;
+#ifdef DEBUG
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+#else
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+  
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  self.window.backgroundColor = [UIColor whiteColor];
+  [[RCCManager sharedInstance] initBridgeWithBundleURL:jsCodeLocation launchOptions:launchOptions];
+  return YES;
+}
+
+@end
