bd5fa36e9f8d4327ca13a6b58e7c29745308d3ed
Piyush <piyush@flocare.health>
2018-05-30 14:18:34 +0530

    welcome screen: typo fix; design fix for android

--- app/init_screens/WelcomePageScreen.js
+++ app/init_screens/WelcomePageScreen.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Button} from 'react-native-elements';
 import firebase from 'react-native-firebase';
-import {StyleSheet, Text, View, ScrollView, Image} from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 import {PrimaryFontFamily, screenNames} from '../utils/constants';
 import {Images} from '../Images';
 
@@ -20,48 +20,50 @@ export class WelcomePageScreen extends Component {
 
   render() {
     return (
-      <View style={{flex: 1, justifyContent: 'space-between'}}>
-        <View>
-          <Text style={styles.headerText}>Be more efficient and organized</Text>
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, justifyContent: 'center', marginTop: 10, marginBottom: 10}}>
+          <View>
+            <Text style={styles.headerText}>Be more efficient and organized</Text>
+          </View>
+          <View>
+            <Image
+              style={styles.stretch}
+              source={Images.welcomeScreen}
+            />
+          </View>
+          <View style={styles.instructionStyle}>
+            <Image
+              style={styles.sectionStretch}
+              source={Images.planDay}
+            />
+            <Text style={styles.bulletPointsStyle}>Plan your day better by having information at your tips</Text>
+          </View>
+          <View style={styles.instructionStyle}>
+            <Image
+              style={styles.sectionStretch}
+              source={Images.viewMap}
+            />
+            <Text style={styles.bulletPointsStyle}>View your visits on a map, follow a shorter route and save time</Text>
+          </View>
+          <View style={styles.instructionStyle}>
+            <Image
+              style={styles.sectionStretch}
+              source={Images.secureData}
+            />
+            <Text style={styles.bulletPointsStyle}>Be secure with auto lock and data encryption</Text>
+          </View>
         </View>
-        <View>
-          <Image
-            style={styles.stretch}
-            source={Images.welcomeScreen}
-          />
-        </View>
-        <View style={styles.instructionStyle}>
-          <Image
-            style={styles.sectionStretch}
-            source={Images.planDay}
-          />
-          <Text style={styles.bulletPointsStyle}>Plan your day better by having information at your tips</Text>
-        </View>
-        <View style={styles.instructionStyle}>
-          <Image
-            style={styles.sectionStretch}
-            source={Images.viewMap}
-          />
-          <Text style={styles.bulletPointsStyle}>View your visits on map and map a better routing</Text>
-        </View>
-        <View style={styles.instructionStyle}>
-          <Image
-            style={styles.sectionStretch}
-            source={Images.secureData}
-          />
-          <Text style={styles.bulletPointsStyle}>Be secure with auto lock and data encryption</Text>
-        </View>
-        <Button
-            containerViewStyle={{marginLeft: 0, marginRight: 0}}
-            buttonStyle={styles.buttonStyle}
-            textStyle={{
+      <Button
+          containerViewStyle={{marginLeft: 0, marginRight: 0}}
+          buttonStyle={styles.buttonStyle}
+          textStyle={{
             fontFamily: PrimaryFontFamily,
             fontSize: 16
-            }}
-            title='Explore' onPress={this.onButtonPress.bind(this)}
-        >
-          Explore
-        </Button>
+          }}
+          title='Explore' onPress={this.onButtonPress.bind(this)}
+      >
+        Explore
+      </Button>
       </View>
     );
   }
@@ -109,6 +111,7 @@ const styles = StyleSheet.create({
   buttonStyle: {
     backgroundColor: '#45ceb1',
     marginLeft: 0,
-    marginRight: 0
+    marginRight: 0,
+    height: 50
   }
 });
