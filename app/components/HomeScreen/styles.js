f8fc7fa0bdbb53e2c8b882cc840bdf116389dc48
nikhil.vavs <nikhil@flocare.health>
2018-07-27 17:37:47 +0530

    Layout and background color change in home screen

---
+++ app/components/HomeScreen/styles.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+    headerStyle: {
+        justifyContent: 'center',
+        marginTop: 20
+    },
+    textStyle: {
+        fontSize: 11,
+        color: '#999999'
+    },
+    listViewIconStyle: {
+        marginLeft: 10,
+        marginRight: 10
+    }
+});
