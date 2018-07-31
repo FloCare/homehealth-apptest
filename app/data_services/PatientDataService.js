af72afa531f4edf849d33ee0abb199ef2785538f
harshal <harshal@flocare.health>
2018-07-31 15:57:56 +0530

    Typo fix and package-lock update

--- app/data_services/PatientDataService.js
+++ app/data_services/PatientDataService.js
@@ -40,7 +40,7 @@ export class PatientDataService {
             primaryContact: patient.primaryContact,
             notes: patient.notes,
             //TODO this will need work if more than one episode per patient
-            visits: VisitService.getInstance().selectCurrentUserVisits(
+            visits: VisitService.getInstance().filterUserVisits(
                 patient.episodes[0].visits).map(visit => visit.visitID),
             archived: patient.archived,
             recentlyAssigned: moment.utc(patient.assignmentTimestamp).diff(moment.utc(), 'days') === 0,
