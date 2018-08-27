fe1b6ded3ebe4ae3fc396e2c65ea7af1c8d066eb
harshal <harshal@flocare.health>
2018-08-27 18:43:48 +0530

    added firebase messaging and notification for android

--- app/data_services/MessagingServices/NotificationService.js
+++ app/data_services/MessagingServices/NotificationService.js
@@ -1,71 +1,16 @@
-import PushNotification from 'react-native-push-notification';
-import {AppState, PushNotificationIOS} from 'react-native';
+import {AppState, Platform} from 'react-native';
 import moment from 'moment/moment';
-import {MessagingServiceCoordinator} from './PubNubMessagingService/MessagingServiceCoordinator';
-import {screenNames} from '../../utils/constants';
 import {todayMomentInUTCMidnight} from '../../utils/utils';
-import {dateService} from '../DateService';
-import {getItem} from '../../utils/InMemoryStore';
+import {configureNotificationAndroid, displayLocalNotificationAndroid} from './AndroidNotificationService';
+import {configureNotificationIOS, displayLocalNotificationIOS} from './IOSNotificationService';
+import {screenNames} from '../../utils/constants';
 
 export function configure() {
     AppState.addEventListener('change', nextState => console.log(nextState));
-    PushNotification.configure({
-        onRegister(notificationTokenObject) {
-            const messagingServiceCoordinator = MessagingServiceCoordinator.getInstance();
-            if (messagingServiceCoordinator) messagingServiceCoordinator.onNotificationRegister(notificationTokenObject);
-        },
-        onNotification(notification) {
-            console.log('NOTIFICATION:', notification);
-            const backgroundTaskTimer = new Promise(resolve => {
-                setTimeout(() => {
-                    console.log('30 seconds elapsed');
-                    resolve(false);
-                }, 30 * 1000);
-            });
-
-            if (notification.data.remote && !notification.foreground) {
-                const notificationPromise = MessagingServiceCoordinator.getInstance().onNotification(notification);
-                Promise.race([notificationPromise, backgroundTaskTimer])
-                    .then((result) => {
-                        console.log('Reached promise then');
-                        console.log(result);
-                        if (result === false) {
-                            notification.finish(PushNotificationIOS.FetchResult.ResultFailed);
-                        }
-                        notification.finish(PushNotificationIOS.FetchResult.NewData);
-                    })
-                    .catch(error => {
-                        console.log('Error occurred during background fetch');
-                        console.log(error);
-                    });
-                return;
-            } else if (!notification.data.remote && !notification.foreground) {
-                if (notification.data.payload.midnightEpoch) dateService.setDate(notification.data.payload.midnightEpoch);
-                const navigator = getItem('navigator');
-                if (navigator) {
-                    navigator.push({
-                        screen: notification.data.navigateTo,
-                        // passProps: {...notification.data.payload, navigator: getItem('navigator')},
-                        passProps: {...notification.data.payload},
-                        navigatorStyle: {
-                            tabBarHidden: notification.data.tabBarHidden
-                        }
-                    });
-                }
-            }
-
-            notification.finish(PushNotificationIOS.FetchResult.NoData);
-        },
-
-        permissions: {
-            alert: true,
-            sound: true
-        },
-
-        popInitialNotification: true,
-
-        requestPermissions: true,
-    });
+    Platform.select({
+        ios: configureNotificationIOS,
+        android: configureNotificationAndroid
+    })();
 }
 
 export function showVisitCollisionNotification(myVisit, collidingVisit) {
@@ -75,9 +20,12 @@ export function showVisitCollisionNotification(myVisit, collidingVisit) {
 
     const myVisitTime = myVisit.plannedStartTime ? moment(myVisit.plannedStartTime).format('LT') : undefined;
 
-    PushNotification.localNotification({
-        message: `${coworkerName} added a visit for ${today ? 'today' : dateString}. You too are visiting the patient ${today ? 'today' : `on ${dateString}`}${myVisitTime ? ` at ${myVisitTime}` : '. Set a time for the visit.'}`,
-        userInfo: {
+    Platform.select({
+        ios: displayLocalNotificationIOS,
+        android: displayLocalNotificationAndroid
+    })({
+        body: `${coworkerName} added a visit for ${today ? 'today' : dateString}. You too are visiting the patient ${today ? 'today' : `on ${dateString}`}${myVisitTime ? ` at ${myVisitTime}` : '. Set a time for the visit.'}`,
+        data: {
             navigateTo: screenNames.visitDayViewScreen,
             tabBarHidden: true,
             payload: {
@@ -85,6 +33,7 @@ export function showVisitCollisionNotification(myVisit, collidingVisit) {
                 visitID: myVisit.visitID,
                 midnightEpoch: myVisit.midnightEpochOfVisit,
             }
-        }
+        },
+        notificationID: `${myVisit.visitID}_${collidingVisit.visitID}_${Date.now()}`
     });
 }
--- app/data_services/MessagingServices/PubNubMessagingService/BaseMessagingService.js
+++ app/data_services/MessagingServices/PubNubMessagingService/BaseMessagingService.js
@@ -1,5 +1,4 @@
 import PubNub from 'pubnub';
-import {AsyncStorage, Platform} from 'react-native';
 import {pubnubPubKey, pubnubSubKey} from '../../../utils/constants';
 import {MessagingServiceCoordinator} from './MessagingServiceCoordinator';
 import {UserDataService} from '../../UserDataService';
@@ -8,7 +7,7 @@ export class BaseMessagingService {
     deviceToken = null;
     caughtUpOnHistory = false;
     channels = [];
-    notificationToken;
+    notificationTokenObject;
 
     onMessage() {
 
@@ -37,33 +36,33 @@ export class BaseMessagingService {
         for (const channel of channels) {
             const assignedVisitLastTimestamp = channel.lastMessageTimestamp;
             const newPromise = this.newClient().history({
-                    channel: channel.name,
-                    reverse: true,
-                    start: assignedVisitLastTimestamp,
-                    stringifiedTimeToken: true,
-                }).then(async response => {
-                    console.log('processFromHistory response');
-                    console.log(response);
-                    for (const message of response.messages) {
-                        try {
-                            await this.digestMessage({
-                                message: message.entry,
-                                timestamp: message.timetoken,
-                                channel: channel.name,
-                                suppressNotification
-                            });
-                        } catch (e) {
-                            console.log('skipping a message that threw error');
-                        }
+                channel: channel.name,
+                reverse: true,
+                start: assignedVisitLastTimestamp,
+                stringifiedTimeToken: true,
+            }).then(async response => {
+                console.log(`processFromHistory response for channel ${channel.name}`);
+                console.log(response);
+                for (const message of response.messages) {
+                    try {
+                        await this.digestMessage({
+                            message: message.entry,
+                            timestamp: message.timetoken,
+                            channel: channel.name,
+                            suppressNotification
+                        });
+                    } catch (e) {
+                        console.log('skipping a message that threw error');
                     }
+                }
 
-                    if (response.messages.length === 100) {
-                        this.processFromHistory([channel]);
-                    }
-                }).catch(error => {
-                    console.log('error in history call');
-                    console.log(error);
-                });
+                if (response.messages.length === 100) {
+                    this.processFromHistory([channel]);
+                }
+            }).catch(error => {
+                console.log('error in history call');
+                console.log(error);
+            });
             allPromises.push(newPromise);
         }
         await Promise.all(allPromises);
@@ -164,7 +163,7 @@ export class BaseMessagingService {
         const liveChannelObjects = this.saveChannelToRealm(channelObjects);
         this.channels.push(...liveChannelObjects);
         this._startSubscription(liveChannelObjects, suppressNotificationFromHistory);
-        this.registerDeviceOnChannels(this.notificationToken, liveChannelObjects);
+        this.registerDeviceOnChannels(this.notificationTokenObject, liveChannelObjects);
     }
 
     //TODO unsubscribe from channels will aslo have to remove notification registration
@@ -184,7 +183,7 @@ export class BaseMessagingService {
         // console.log(`${channelObjects.length},${liveChannelsToRemove.length},${this.channels.length}`);
 
         this._unsubscribe(liveChannelsToRemove);
-        this.deregisterDeviceOnChannels(this.notificationToken, liveChannelsToRemove);
+        this.deregisterDeviceOnChannels(this.notificationTokenObject, liveChannelsToRemove);
         this.channels = this.channels.filter(channel => liveChannelsToRemove.indexOf(channel) === -1);
         this.deleteChannelsFromRealm(channelObjects.map(channelObject => channelObject.name));
     }
@@ -222,36 +221,46 @@ export class BaseMessagingService {
 
 
     onNotificationRegister(notificationTokenObject) {
-        this.notificationToken = notificationTokenObject.token;
-        this.registerDeviceOnChannels(notificationTokenObject.token);
+        this.notificationTokenObject = notificationTokenObject;
+        this.registerDeviceOnChannels(notificationTokenObject);
     }
 
-    registerDeviceOnChannels(token, channels = this.channels) {
-        if (Platform.OS === 'android') {
+    registerDeviceOnChannels(tokenObject, channels = this.channels) {
+        console.log('register device on channels args');
+        console.log(tokenObject);
+        if (!tokenObject) {
             return;
         }
 
         return this.pubnub.push.addChannels(
             {
                 channels: channels.map(channel => channel.name),
-                device: token,
-                pushGateway: 'apns'
+                device: tokenObject.token,
+                pushGateway: tokenObject.pushGateway
             }
-        );
+        ).then(result => {
+            console.log('result of registering device on channel');
+            console.log(result);
+        }).catch(error => {
+            console.log('error in registering device on channel');
+            console.log(error);
+        });
     }
 
-    deregisterDeviceOnChannels(token, channels) {
-        if (Platform.OS === 'android') {
-            return;
-        }
-
+    deregisterDeviceOnChannels(tokenObject, channels) {
         return this.pubnub.push.removeChannels(
             {
                 channels: channels.map(channel => channel.name),
-                device: token,
-                pushGateway: 'apns'
+                device: tokenObject.token,
+                pushGateway: tokenObject.pushGateway
             }
-        );
+        ).then(result => {
+            console.log('result of deregistering device on channel');
+            console.log(result);
+        }).catch(error => {
+            console.log('error in deregistering device on channel');
+            console.log(error);
+        });
     }
 
     _startSubscription(channels, suppressNotificationFromHistory = false) {
