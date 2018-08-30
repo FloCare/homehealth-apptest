636d37eabb33de1e420c78f9090d95eb7d7be4a7
harshal <harshal@flocare.health>
2018-08-30 17:25:23 +0530

    task data service to create and mark tasks as done

--- app/components/HomeScreen/TaskSection.js
+++ app/components/HomeScreen/TaskSection.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Dimensions, View, TouchableOpacity} from 'react-native';
 import {styles} from '../common/styles';
 import {CustomCheckBox} from '../common/CustomCheckBox';
 import StyledText from '../common/StyledText';
+import {TaskService} from '../../data_services/TaskService';
 
-function taskCard(task, onPress) {
+function taskCard(task, onPressCard, checkBoxDisabled = false) {
     return (
-        <View style={{flexDirection: 'row', marginRight: 10, width: 0.95 * Dimensions.get('screen').width, marginVertical: 3}}>
+        <View
+            style={{
+                flexDirection: 'row',
+                marginRight: 10,
+                width: 0.95 * Dimensions.get('screen').width,
+                marginVertical: 3
+            }}
+        >
             <View style={{justifyContent: 'space-around'}}>
                 <CustomCheckBox
-                    checked={false}
-                    onPress={() => {}}
+                    checked={task.isDone}
+                    disabled={checkBoxDisabled}
+                    onPress={() => TaskService.getInstance().flipTaskDoneStatus(task.taskID)}
                     checkBoxStyle={{width: 20, height: 20, alignSelf: 'flex-start', marginTop: 10}}
                     checkBoxContainerStyle={{width: 40, justifyContent: 'center'}}
                 />
             </View>
             <TouchableOpacity
-                onPress={onPress}
+                onPress={onPressCard}
                 style={[
                     styles.cardContainerStyle,
                     {flex: 8, marginTop: 2, marginBottom: 2, flexDirection: 'row', shadowRadius: 1}
@@ -28,29 +37,59 @@ function taskCard(task, onPress) {
                     </StyledText>
                     {
                         task.patientName
-                            && <StyledText
-                                    style={{fontSize: 9, fontWeight: '300'}}
-                            >
-                                    {task.patientName}
-                                </StyledText>
+                        && <StyledText
+                            style={{fontSize: 9, fontWeight: '300'}}
+                        >
+                            {task.patientName}
+                        </StyledText>
                     }
                 </View>
             </TouchableOpacity>
         </View>
     );
 }
-export function TaskSection(props) {
-    return (
-        <View style={{flex: 1, marginVertical: 20, alignItems: 'center'}} >
-            <StyledText
-                style={{fontSize: 12}}
-            >
-                {`My Task ${props.tasks.length}`}
-            </StyledText>
 
-            {
-                 props.tasks.map(task => taskCard(task))
-            }
-        </View>
-    );
+export class TaskSection extends Component {
+    constructor(props) {
+        super(props);
+        this.subscriber = TaskService.getInstance().subscribeToFlatTasksForDay(props.midnightEpoch, this.setTasks.bind(this));
+        this.state = {};
+    }
+
+    componentWillUnmount() {
+        this.subscriber();
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.midnightEpoch !== this.props.midnightEpoch) {
+            this.subscriber();
+            this.setState({});
+            this.subscriber = TaskService.getInstance().subscribeToFlatTasksForDay(nextProps.midnightEpoch, this.setTasks.bind(this));
+        }
+    }
+
+    setTasks(tasks) {
+        this.setState({tasks});
+    }
+
+    render() {
+        if ((!this.state.tasks || this.state.tasks.length === 0) && (this.props.dateMinusToday < 0)) {
+            return null;
+        }
+        return (
+            <View style={{flex: 1, marginVertical: 20, alignItems: 'center'}}>
+                <StyledText
+                    style={{fontSize: 12}}
+                >
+                    {'My Task'}
+                </StyledText>
+                {
+                    this.props.dateMinusToday >= 0 ? taskCard({body: '+ Add Task'}, () => TaskService.getInstance().createNewTask(`yoo${Math.random()}`, this.props.midnightEpoch), true) : undefined
+                }
+                {
+                    this.state.tasks ? this.state.tasks.map(task => taskCard(task)) : undefined
+                }
+            </View>
+        );
+    }
 }
