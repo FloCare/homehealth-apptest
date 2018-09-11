3bbb58649e26976e89a9abc0b56687fac5917585
harshal <harshal@flocare.health>
2018-09-11 19:28:04 +0530

    Added badge to notification center and clearing of it after visiting the screen

--- app/components/NotificationScreen/NotificationScreen.js
+++ app/components/NotificationScreen/NotificationScreen.js
@@ -1,80 +1,10 @@
 import moment from 'moment/moment';
 import React, {Component} from 'react';
-import {View, TouchableOpacity, SectionList} from 'react-native';
+import {View, TouchableOpacity, SectionList, AsyncStorage} from 'react-native';
 import {PrimaryColor} from '../../utils/constants';
 import StyledText from '../common/StyledText';
 import styles from '../common/SectionedList/styles';
 import {NotificationService} from '../../data_services/MessagingServices/NotificationService';
-
-function renderNotification({item}) {
-    const daysDiff = moment().diff(item.createdAt, 'days');
-    const hoursDiff = moment().diff(item.createdAt, 'hours');
-
-    let timeSinceNotification;
-    if (daysDiff === 0) {
-        if (hoursDiff !== 0) {
-            timeSinceNotification = `${hoursDiff} hours ago`;
-        } else timeSinceNotification = 'Right now';
-    } else {
-        timeSinceNotification = `${daysDiff} days ago`;
-    }
-
-    return (
-        <TouchableOpacity
-            style={{
-                marginVertical: 3,
-                paddingRight: 20
-            }}
-        >
-            <View
-                style={{
-                    flexDirection: 'row'
-                }}
-            >
-                <View
-                    style={{
-                        flex: 1,
-                        alignItems: 'center',
-                        justifyContent: 'space-around'
-                    }}
-                >
-                    <View
-                        style={{
-                            backgroundColor: item.isSeen ? 'clear' : PrimaryColor,
-                            borderRadius: 7,
-                            height: 7,
-                            width: 7
-                        }}
-                    />
-                </View>
-                <View
-                    style={{
-                        flex: 6
-                    }}
-                >
-                    <StyledText
-                        style={{
-                            fontSize: 15,
-                            color: 'black',
-                            marginVertical: 5
-                        }}
-                    >
-                        {item.body}
-                    </StyledText>
-                    <StyledText
-                        style={{
-                            fontSize: 11,
-                            color: 'grey',
-                            marginVertical: 3
-                        }}
-                    >
-                        {timeSinceNotification}
-                    </StyledText>
-                </View>
-            </View>
-        </TouchableOpacity>
-    );
-}
 
 function renderSectionHeader({section}) {
     return (
@@ -87,11 +17,19 @@ export class NotificationScreen extends Component {
         super(props);
         this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
         this.state = {};
-        // props.navigator.setTabBadge({
-        //     badge: 17, // badge value, null to remove badge
-        //     badgeColor: '#006400', // (optional) if missing, the badge will use the default color
-        // });
+        AsyncStorage.getItem('lastSeenNotificationTimestamp').then(lastSeenNotificationTimestamp => {
+           this.setState({lastSeenNotificationTimestamp: lastSeenNotificationTimestamp ? parseInt(lastSeenNotificationTimestamp) : 0});
+        });
+
         NotificationService.getInstance().subscribeToNotification(this.onNotificationRefresh.bind(this));
+    }
+
+    componentDidMount() {
+        this.refreshTabBadge();
+    }
+
+    componentDidUpdate() {
+        this.refreshTabBadge();
     }
 
     onNotificationRefresh(notifications) {
@@ -103,7 +41,6 @@ export class NotificationScreen extends Component {
             title: 'Earlier',
             data: []
         };
-
 
         notifications.forEach(notification => {
             const ageInDays = moment().diff(notification.createdTime / 10000, 'days');
@@ -131,15 +68,106 @@ export class NotificationScreen extends Component {
         }
     }
 
-    willDisappear() {
+    refreshTabBadge() {
+        const notificationSections = this.state.notificationSections;
+        let freshCount = 0;
+        if (notificationSections) {
+            notificationSections.forEach(notificationSection => {
+                notificationSection.data.forEach(notification => {
+                    if (notification.createdTime > this.state.lastSeenNotificationTimestamp) {
+                        freshCount++;
+                    }
+                });
+            });
+        }
 
+        this.props.navigator.setTabBadge({
+            badge: freshCount !== 0 ? freshCount : null, // badge value, null to remove badge
+            badgeColor: '#006400', // (optional) if missing, the badge will use the default color
+        });
+    }
+
+    willDisappear() {
+        const mostRecentNotification = this.state.notificationSections[0].data[0] || this.state.notificationSections[0].data[1];
+        AsyncStorage.setItem('lastSeenNotificationTimestamp', mostRecentNotification.createdTime.toString());
+        this.setState({lastSeenNotificationTimestamp: mostRecentNotification.createdTime});
+    }
+
+    renderNotification({item}) {
+        const daysDiff = moment().diff(item.createdTime / 10000, 'days');
+        const hoursDiff = moment().diff(item.createdTime / 10000, 'hours');
+
+        let timeSinceNotification;
+        if (daysDiff === 0) {
+            if (hoursDiff !== 0) {
+                timeSinceNotification = `${hoursDiff} hour${hoursDiff > 1 ? 's' : ''} ago`;
+            } else timeSinceNotification = 'Right now';
+        } else {
+            timeSinceNotification = `${daysDiff} day${daysDiff > 1 ? 's' : ''} ago`;
+        }
+
+        return (
+            <TouchableOpacity
+                style={{
+                    marginVertical: 3,
+                    paddingRight: 20
+                }}
+            >
+                <View
+                    style={{
+                        flexDirection: 'row'
+                    }}
+                >
+                    <View
+                        style={{
+                            flex: 1,
+                            alignItems: 'center',
+                            justifyContent: 'space-around'
+                        }}
+                    >
+                        <View
+                            style={{
+                                backgroundColor: item.createdTime <= this.state.lastSeenNotificationTimestamp ? 'transparent' : PrimaryColor,
+                                borderRadius: 7,
+                                height: 7,
+                                width: 7
+                            }}
+                        />
+                    </View>
+                    <View
+                        style={{
+                            flex: 6
+                        }}
+                    >
+                        <StyledText
+                            style={{
+                                fontSize: 15,
+                                color: 'black',
+                                marginVertical: 5
+                            }}
+                        >
+                            {item.body}
+                        </StyledText>
+                        <StyledText
+                            style={{
+                                fontSize: 11,
+                                color: 'grey',
+                                marginVertical: 3
+                            }}
+                        >
+                            {timeSinceNotification}
+                        </StyledText>
+                    </View>
+                </View>
+            </TouchableOpacity>
+        );
     }
 
     render() {
         if (this.state.notificationSections) {
             return (
                 <SectionList
-                    renderItem={renderNotification}
+                    renderItem={this.renderNotification.bind(this)}
                     renderSectionHeader={renderSectionHeader}
                     sections={this.state.notificationSections}
                 />
