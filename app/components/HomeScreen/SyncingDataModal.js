d62bc8a678701787206dbd4f738ec12c05f3dfc2
nikhil.vavs <nikhil@flocare.health>
2018-10-24 12:04:47 +0530

    Changing sync modal

--- app/components/HomeScreen/SyncingDataModal.js
+++ app/components/HomeScreen/SyncingDataModal.js
@@ -6,11 +6,13 @@ export default class SyncingDataModal extends Component {
 
     render() {
         return (
-            <View style={{margin: 10, paddingTop: 20, paddingBottom: 20, backgroundColor: 'white', borderRadius: 5, alignItems: 'center'}}>
-                <ActivityIndicator size='small' color="black" />
-                <Text style={{...styles.textStyle, marginTop: 10}}>
-                    Syncing Data From Server
-                </Text>
+            <View style={{alignItems: 'center'}}>
+                <View style={{padding: 10, paddingTop: 20, paddingBottom: 20, backgroundColor: 'white', borderRadius: 5}}>
+                    <ActivityIndicator size='small' color="black" />
+                    <Text style={{...styles.textStyle, marginTop: 10, color: 'black'}}>
+                        Syncing Data From Server
+                    </Text>
+                </View>
             </View>
         );
     }
