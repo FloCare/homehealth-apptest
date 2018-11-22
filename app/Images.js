e995e5e9de11549c425f452ac3876290858a5afa
harshal <harshal@flocare.health>
2018-11-22 15:12:41 +0530

    notes assets

--- app/Images.js
+++ app/Images.js
@@ -23,6 +23,7 @@ export const Images = {
     mapView: require('../resources/mapView.png'),
     milesLog: require('../resources/milesLog.png'),
     more: require('../resources/more.png'),
+    notesIcon: require('../resources/notesIcons.png'),
     notificationBell: require('../resources/notification.png'),
     navigate: require('../resources/navigate.png'),
     notes: require('../resources/notes.png'),
